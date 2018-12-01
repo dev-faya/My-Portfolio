cd0565d0936ef695d58c3894b879776b723d92a5
dev-faya <toumi7366@gmail.com>
2018-12-01 11:21:38 +0000

    edit banner-text

---
+++ build/precache-manifest.83cbd28f4fb6814938c572c1974cd235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "9fe9a913d3d8da21a902",
+    "url": "/my-portfolio/static/js/main.9fe9a913.chunk.js"
+  },
+  {
+    "revision": "b6f1e28920ec96fe93c2",
+    "url": "/my-portfolio/static/js/1.b6f1e289.chunk.js"
+  },
+  {
+    "revision": "9fe9a913d3d8da21a902",
+    "url": "/my-portfolio/static/css/main.0579eb07.chunk.css"
+  },
+  {
+    "revision": "b6f1e28920ec96fe93c2",
+    "url": "/my-portfolio/static/css/1.4c7fb592.chunk.css"
+  },
+  {
+    "revision": "07815395fdbf22cdc17972019cfba817",
+    "url": "/my-portfolio/index.html"
+  }
+];
--- src/components/home.js
+++ src/components/home.js
@@ -1,64 +1,64 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl'
+import { Container, Row, Col } from 'reactstrap';
 import Hnavbar from './navbar';
 
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="landing-grid">
         <Hnavbar />
-        <div className="home-section" id="home">
-            <Grid className="landing-grid">
-                <Cell col={12} >
+        <div className="home-section" id="home" style={{paddingBottom: "50px"}}>
+            <Container>
                     <img 
                         src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
                         alt="avatar-pic"
                         className="avatar-img"
                     />
                     
-                    <div className="banner-text" style={{marginBottom: "10%"}}>
-                        <Cell col={12}>
-                            <Cell col={12}>
+                    <div className="banner-text">
+                        <Col md="12" sm="12">
+                            <Col md="12" sm="12">
                                 <h1>Front-End Web Developer</h1>
-                            </Cell>
+                            </Col>
 
                             <hr />
                             <p>HTML/CSS | Bootstrap | Javascript | React | JQuery</p>
-                        </Cell>
+                        </Col>
 
-                        {/* LinkedIn */}
-                        <Cell col={12} className="social-links">
-                            <Cell col={3}>
-                                <a href="#home" target="_blank">
-                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
-                                </a>
-                            </Cell>
+                        <Col>
+                            <Row className="social-links">
+                                {/* LinkedIn */}
+                                <Col xs="3" md="3">
+                                    <a href="#home" target="_blank">
+                                        <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                    </a>
+                                </Col>
 
-                        {/* GitHub */}
-                            <Cell col={3}>
-                                <a href="#home" target="_blank">
-                                    <i className="fa fa-github-square" aria-hidden="true" />
-                                </a>
-                            </Cell>
+                            {/* GitHub */}
+                                <Col xs="3" md="3">
+                                    <a href="#home" target="_blank">
+                                        <i className="fa fa-github-square" aria-hidden="true" />
+                                    </a>
+                                </Col>
 
-                        {/* Freecodecam */}
-                            <Cell col={3}>
-                                <a href="#home" target="_blank">
-                                    <i className="fa fa-free-code-camp" aria-hidden="true" />
-                                </a>
-                            </Cell>
+                            {/* Freecodecam */}
+                                <Col xs="3" md="3">
+                                    <a href="#home" target="_blank">
+                                        <i className="fa fa-free-code-camp" aria-hidden="true" />
+                                    </a>
+                                </Col>
 
-                        {/* Youtube */}
-                            <Cell col={3}>
-                                <a href="#home" target="_blank">
-                                    <i className="fa fa-youtube-square" />
-                                </a>
-                            </Cell>
-                        </Cell>
+                            {/* Youtube */}
+                                <Col xs="3" md="3">
+                                    <a href="#home" target="_blank">
+                                        <i className="fa fa-youtube-square" />
+                                    </a>
+                                </Col>
+                            </Row>
+                        </Col>
                     </div>
-                </Cell>
-            </Grid>
+            </Container>
         </div>
       </div>
     )
